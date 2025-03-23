47594e88f399f392c1fc67f21cd19c60eeb6afe5
Lillith Fox <imlvnaa@gmail.com>
2025-03-23 14:06:17 -0400

    c/i build

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 A simple program to swap between pishock and openshock easily, while saving settings!
 
-exe coming soon™
+## Downloads
+
+Downloads can either be found [Here](https://nightly.link/lillithkt/pishock-switcher/workflows/build/main/Pishock-Switcher.exe.zip) if you are not logged in to github, or [here](https://github.com/lillithkt/pishock-switcher/actions/workflows/build.yml) if you are
--- build.mjs
+++ build.mjs
@@ -1,14 +1,23 @@
 import * as esbuild from "esbuild";
+import { exec } from "pkg";
 
-esbuild.build({
-  entryPoints: ["src/index.ts"],
-  bundle: true,
-  outfile: "dist/index.js",
-  platform: "node",
-  target: ["node21"],
-  sourcemap: true,
-  external: ["serialport"],
-  loader: {
-    ".node": "copy",
-  },
-});
+(async () => {
+  await esbuild.build({
+    entryPoints: ["src/index.ts"],
+    bundle: true,
+    outfile: "dist/index.js",
+    platform: "node",
+    target: ["node21"],
+    sourcemap: true,
+    external: ["serialport"],
+    loader: {
+      ".node": "copy",
+    },
+  });
+
+  if (process.argv.includes("--exe")) {
+    console.log("Packaging...");
+    await exec(["package.json", "--output", "dist/PiShock-ButtplugIO.exe"]);
+  }
+  console.log("Done!");
+})();
